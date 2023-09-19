5faca70708115851ee8c2ad76cb18eef2f5ef1d2
Alex Mattoni <43799350+alexmattoni@users.noreply.github.com>
2023-09-19 18:45:11 -0400

    branching to dev

---
+++ front-end/src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import type { LayoutData } from './$types';
+
+    export let data: LayoutData;
+</script>
+
+<nav class="navbar navbar-collapse">
+    <a class="navbar-brand">Navbar</a>
+
+</nav>
--- front-end/src/routes/+page.svelte
+++ front-end/src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to Call Tracker</h1>
+<p>This will display trunked radio messages scanned on our SDR located in Troy, NY.</p>
