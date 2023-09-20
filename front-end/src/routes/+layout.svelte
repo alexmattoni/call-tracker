603f1de3338beb3c153d1819ee2e837307f5e089
Alex Mattoni <43799350+alexmattoni@users.noreply.github.com>
2023-09-19 20:14:54 -0400

    navbar update

--- front-end/src/routes/+layout.svelte
+++ front-end/src/routes/+layout.svelte
@@ -6,11 +6,10 @@
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </svelte:head>
 
-<nav class="navbar navbar-expand-lg bg-dark navbar-dark mb-4 px-4 justify-content-right flex-nowrap">
+<nav class="navbar navbar-expand-lg bg-dark navbar-dark mb-4 px-4">
         <a class="navbar-brand" href="/">Call Tracker</a>
-        
-        <div class="d-flex justify-content-right">
-            <a class="btn btn-outline-success me-2" type="button" href="/">home</a>
+        <div class="container justify-content-end">
+            <a class="btn btn-outline-success me-3 my-2" type="button" href="/">home</a>
             <a class="btn btn-outline-danger" type="button" href="/tracking">tracking</a>
         </div>
 </nav>
